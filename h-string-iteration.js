92040eb8a5109ac8a8aa4f900838a03461cccc13
Jasmine Davis <jasminedavis@Jasmines-MBP.home>
2020-12-23 10:12:32 -0500

    added new kata h

--- h-string-iteration.js
+++ h-string-iteration.js
@@ -1,8 +1,31 @@
-var str = "Jasmine"
+var str = "Hello"
+var splitStr = str.split('')
+console.log(splitStr)
+
+str = "Howdy"
+var spreadStr = [...str]
+console.log(spreadStr)
+
+str = "Bonjour"
+var arrayStr = Array.from(str)
+console.log(arrayStr)
+
+
+str = "Jasmine"
 var x
 
 for (x of str) {
     console.log(x)
 }
 
-// Will pick this up tomorrow :-)
+
+str = "Pete"
+const arr = [...str]
+arr.forEach(element => console.log(element));
+
+
+
+// Things I learned:
+// .forEach() can ONLY be used on arrays, so you have to first convert your string to an array
+// Spread "[...]" and Array.from are new to me!
+// for/of and for/in are easy to get confused! ("of" gives you the actual value, "in" gives you the key/index)
